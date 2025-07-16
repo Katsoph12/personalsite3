f47cf8854c18cba1f5bed699042481aecc8d6780
Katsoph12 <Catchxaxtiger@gmail.com>
2025-07-16 12:05:54 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PersonalWebsite from "./PersonalWebsite";
+import "./index.css";
+import "@fontsource-variable/inter";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PersonalWebsite />
+  </React.StrictMode>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1F2937',
+        accent: '#3B82F6',
+      },
+      fontFamily: {
+        sans: ['InterVariable', 'sans-serif'],
+        heading: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
